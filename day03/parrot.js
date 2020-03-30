c8e96855e1e2891da8c2af7cb30b3a6540857307
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-03-30 09:49:05 +0700

    Added js file

---
+++ day03/parrot.js
@@ -0,0 +1,12 @@
+class parrotTemplate{
+
+    constructor(name,color,numLegs){
+    this.name = name
+    this.color = color
+    this.numLegs = numLegs
+    }
+}
+talky = new parrotTemplate('Fern','Red-Blue',2)
+console.log(talky.numLegs)
+console.log(talky.name)
+console.log(talky.color)
