6693eaacab4c0029a3fc452336bea0897599f4e3
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-07 09:03:01 +0700

    Udemy alogarithm class

---
+++ day11/obj.js
@@ -0,0 +1,12 @@
+// Object not ordered, arrays are ordered
+let instructor = {
+    firstName:"Kelly",
+    idInstructor:true,
+    favouriteNumbers:[1,2,3,4]
+}
+
+console.log(instructor.firstName)
+console.log(Object.keys(instructor));
+
+let names = ["Michael","Aiea","Melissa"];
+console.log(names[1])
