400c2778d6f79238b8f72975af42886d90360426
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-08 16:07:21 +0700

    modified code

--- day12/slidingwindow2.js
+++ day12/slidingwindow2.js
@@ -1,26 +1,32 @@
-function minSubArrayLen(arr,number){
+function minSubArrayLen(arr, number) {
     let max_num = 0;
     let index = 0;
+    let count = 1;
     //Find maximum number
-    for(let i=0;i<arr.length;i++){
-        if(max_num < arr[i]){
+    for (let i = 0; i < arr.length; i++) {
+        if (max_num < arr[i]) {
             max_num = arr[i];
             index = i;
         }
     }
-    // console.log(index);
-    if(max_num > number){
+    if (max_num > number) {
         return 1;
     }
-    while(index < arr.length-1){
-        if(arr[index+1] > arr[index-1]){
-            max_num = max_num+arr[index+1]; 
-        }else{
-         max_num = max_num+arr[index-1];
-        }
-        index++;
+    if (arr[index + 1] > arr[index - 1]) {
+        max_num = max_num + arr[index + 1];
+        count++;
+        
+    } else {
+        max_num = max_num + arr[index - 1];
+        count++;
     }
-    console.log(max_num);  
+    index++
+    if (max_num >= number) {
+        console.log(max_num);
+        return true;
+    }
+    return false;
 }
-console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19],52));
-console.log(minSubArrayLen([2,3,1,2,4,3],7));
+// console.log(minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19],52));
+// console.log(minSubArrayLen([2,3,1,2,4,3],7));
+console.log(minSubArrayLen([2, 1, 6, 5, 4], 9));
