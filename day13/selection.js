21c305fa81a70ffafbd401fc630b1ca695dd053c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-09 10:39:34 +0700

    Updated with the solution

--- day13/selection.js
+++ day13/selection.js
@@ -1,19 +1,17 @@
-function selection(arr){
-    let min_index = 0;
-    let comp_index = 0;
-    let temp = 0;
-    for(let i = min_index;i<arr.length;i++){
-        if(arr[min_index] > arr[i]){
-            comp_index = i;
-        } else{
-            comp_index = min_index;
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var min_index = i;
+        for (var j = i + 1 ; j < arr.length; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            } 
         }
-        arr[temp] = arr[comp_index];
-        arr[comp_index] = arr[min_index];
-        arr[min_index] = arr[temp];
-        
+        var temp= arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
     }
     return arr;
 }
 
-console.log(selection[5,3,3,8,9]);
+console.log(selectionSort([5, 3, 3, 8, 9, 15, 10, 8, 24]));
+console.log(selectionSort([34,22,10,19,17]));
