9883268d1bb0fc85597f2fc80451cdb2a3e78ab0
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-09 10:58:00 +0700

    insertion sort technique

--- day13/selection.js
+++ day13/selection.js
@@ -1,17 +1,20 @@
 function selectionSort(arr) {
     for (var i = 0; i < arr.length; i++) {
         var min_index = i;
-        for (var j = i + 1 ; j < arr.length; j++) {
+        for (var j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[min_index]) {
                 min_index = j;
-            } 
+            }
         }
-        var temp= arr[i];
-        arr[i] = arr[min_index];
-        arr[min_index] = temp;
+        if (i !== min_index) {
+            var temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
+        }
+
     }
     return arr;
 }
 
 console.log(selectionSort([5, 3, 3, 8, 9, 15, 10, 8, 24]));
-console.log(selectionSort([34,22,10,19,17]));
+console.log(selectionSort([34, 22, 10, 19, 17]));
