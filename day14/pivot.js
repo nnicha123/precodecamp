bd7e3e4497cff67f6a0b44e7f9bcf059a60a5fb9
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-10 09:03:53 +0700

    Need recursive function to assist

--- day14/pivot.js
+++ day14/pivot.js
@@ -2,22 +2,35 @@ function pivot(arr, start) {
     let start_index = start;
     let count = 0;
     let j = 0;
-    while (j < 12) {
-        for (let i = 1; i < arr.length; i++) {
-            if (arr[start] > arr[i]) {
-                count++;
-            }
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[start] > arr[i]) {
+            count++;
         }
-        //SWAP
-        let temp = arr[count];
-        arr[count] = arr[start];
-        arr[start] = temp;
-        // console.log(arr, count);
-
-        j++;
-        console.log(j);
     }
-    console.log(arr);
+    //SWAP
+    let temp = arr[count];
+    arr[count] = arr[start];
+    arr[start] = temp;
+    // console.log(arr, count);
+
+    //Keep first count & now look at every element to the left
+    let first_round = count;
+    // console.log(first_round);
+
+    //Left loop //reset count (later can put into recursive function)
+    count = 0;
+    for(let i = 0;i<first_round;i++){
+        if(arr[first_round]>arr[i]){
+            count++
+        }
+    }
+        //SWAP
+        console.log(count);
+        temp = arr[count];
+        arr[count] = arr[first_round];
+        arr[first_round] = temp;
+        console.log(arr);
 }
+
 
 console.log(pivot([28, 41, 4, 11, 16, 1, 40, 14, 36, 37, 42, 18], 0))
