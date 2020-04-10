2b5ab1d1f712b1bc528028cebf080b0bd040c7d8
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-10 10:14:55 +0700

    Modified getDigit into a function

--- day14/getDigit.js
+++ day14/getDigit.js
@@ -1,12 +1,10 @@
-function getDigit(num,digit){
-    
+function getDigit(number, digit) {
+    for (let i = 0; i < digit; i++) {
+        number = parseInt(number / 10);
+    }
+    number = number % 10;
+    return number;
 }
 
-let number = 12345;
-let digit = 1;
-let getdigit = 4;
-for(let i = 0;i<getdigit;i++){
-    number = parseInt(number/10);
-}
-let newnum = number%10;
-console.log(newnum);
+console.log(getDigit(12345,3));
+
