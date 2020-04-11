2fa9e8d07aab5a1e7f218c693850fb71928e3e34
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-11 11:24:38 +0700

    Shift and unshift implementation

---
+++ day15/pop_sol.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this;
+
+    }
+    transverse() {
+        var current = this.head;
+        while (current) {
+            // console.log(current.val);
+            current = current.next;
+        }
+    }
+    pop() {
+        
+        if (!this.head) {
+            return undefined;
+        }
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length ===0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+}
+var newList = new SinglyLinkedList();
+newList.push('Betty');
+newList.push('Alpha');
+newList.push('Candy');
+newList.push('Poop');
+console.log(newList.pop());
+console.log(newList.pop());
+console.log(newList.pop());
+console.log(newList.pop());
+console.log(newList.pop());
