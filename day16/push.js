5b7bd9a5b811955c37b432cdb244c9a4e9e952d8
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-12 11:29:30 +0700

    Doubly linked list push

---
+++ day16/push.js
@@ -0,0 +1,36 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+
+        }
+        this.length++;
+        return this;
+    }
+}
+newList = new DoublyLinkedList();
+console.log(newList);
+newList.push('Betty');
+newList.push('Alpha');
+newList.push('Candy');
+newList.push('Poop');
+console.log(newList);
