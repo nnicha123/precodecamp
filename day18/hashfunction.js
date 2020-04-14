393a7efb6abf054858aa560babaa2af2896b8376
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-14 09:08:07 +0700

    first hash function (fits criteria but needs to be more scattered)

---
+++ day18/hashfunction.js
@@ -0,0 +1,12 @@
+function hash(key,arrayLen){
+    let total = 0;
+    for(let char of key){
+        //map "a" to 1, "b" to 2, etc
+        let value = char.charCodeAt(0) - 96;
+        total = (total + value)%arrayLen;
+    }
+    return total;
+}
+console.log(hash("pink",10));
+console.log(hash("maroon",10));
+console.log(hash("red",10));
