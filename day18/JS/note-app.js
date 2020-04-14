fb9ddf7cc4ed1d189a7290ae80ee3b6d864c9783
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-14 17:35:35 +0700

    Reading and writing HTML using JS (seeing results in console of chrome)

--- day18/JS/note-app.js
+++ day18/JS/note-app.js
@@ -1,9 +1,19 @@
 const p = document.querySelector('p');
 const h2 = document.querySelector('h2');
-
+const h3 = document.querySelectorAll('h3');
 
 //Query and remove
 const ps = document.querySelectorAll('p');
 ps.forEach(function(p){
     p.remove();
+})
+
+const hs = document.querySelectorAll('h2');
+hs.forEach(function(h2){
+    console.log(h2.textContent)
+})
+
+const h3s = document.querySelectorAll('h3');
+h3s.forEach(function(h3){
+    h3.textContent = "YOYOYOYO"
 })
