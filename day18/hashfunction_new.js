986d4eb808849e47c3a61cd871e1189b4622e680
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-14 09:15:11 +0700

    Add prime no. & add prime number to speed up & reduce collision (data more distributed)

---
+++ day18/hashfunction_new.js
@@ -0,0 +1,13 @@
+function hash(key,arrayLen){
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for(let i = 0;i<Math.min(key.length,100);i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        total = (total + WEIRD_PRIME + value)%arrayLen;
+    }
+    return total;
+}
+console.log(hash("pink",10));
+console.log(hash("maroon",10));
+console.log(hash("red",10));
