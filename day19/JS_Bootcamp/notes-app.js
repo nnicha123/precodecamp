acca0f00bd89a8f9963f768460a226e10e4aa83f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-15 11:58:51 +0700

    select & modify based on id and class

--- day19/JS_Bootcamp/notes-app.js
+++ day19/JS_Bootcamp/notes-app.js
@@ -2,11 +2,11 @@ const p = document.querySelector('p');
 const h2 = document.querySelector('h2');
 const h3 = document.querySelectorAll('h3');
 
-//Query and remove
-const ps = document.querySelectorAll('p');
-ps.forEach(function(p){
-    p.remove();
-})
+// //Query and remove
+// const ps = document.querySelectorAll('p');
+// ps.forEach(function(p){
+//     p.remove();
+// })
 
 const hs = document.querySelectorAll('h2');
 hs.forEach(function(h2){
@@ -18,8 +18,14 @@ h3s.forEach(function(h3){
     h3.textContent = "YOYOYOYO"
 })
 
-document.querySelector('button').addEventListener('click',function(e){
+document.querySelector('#create-note').addEventListener('click',function(e){
     console.log('Did this work');
     console.log(e);
     e.target.textContent = 'The button was clicked'
+})
+document.querySelector('#remove-all').addEventListener('click',function(e){
+    console.log('delete all notes')
+    document.querySelectorAll('.note').forEach(function(note){
+        note.remove();
+    });
 })
