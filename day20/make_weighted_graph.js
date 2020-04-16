39f0d78176f0fa658a77db2878b74c5e3657cc93
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-16 08:51:48 +0700

    Review, building graph

---
+++ day20/make_weighted_graph.js
@@ -0,0 +1,25 @@
+class WeightedGraph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = [];
+        }
+    }
+    addEdge(vertex1,vertex2,weight){
+        this.adjacencyList[vertex1].push({node:vertex2,weight});
+        this.adjacencyList[vertex2].push({node:vertex1,weight});
+    }
+}
+var graph = new WeightedGraph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addEdge("A","B",2);
+graph.addEdge("B","C",3);
+graph.addEdge("B","E",2);
+graph.addEdge("C","D",1);
+graph.addEdge("E","C",2);
