9c7acac7f7a8162dac2aa20739b97cc9ae283476
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-16 15:55:49 +0700

    added function so can now click to remove

--- day20/ReactJsAngular_intro/fiddle.js
+++ day20/ReactJsAngular_intro/fiddle.js
@@ -1,17 +1,23 @@
-var inputEl = document.querySelector('input');
-var buttonEl = document.querySelector('button');
-var ulEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addTodo);
 
-function addTodo() {
-  var userInput = inputEl.value
-  if (userInput.trim() == '') {
-    return;
-  }
-  var todoLi = document.createElement('LI');
-  todoLi.textContent = userInput;
-  ulEl.appendChild(todoLi);
-}
+        var inputEl = document.querySelector('input');
+        var buttonEl = document.querySelector('button');
+        var ulEl = document.querySelector('ul');
 
-addTodo()
+        buttonEl.addEventListener('click', addTodo);
+
+        function addTodo() {
+            var userInput = inputEl.value
+            if (userInput.trim() == '') {
+                return;
+            }
+            var todoLi = document.createElement('LI');
+            todoLi.textContent = userInput;
+            todoLi.addEventListener('click',removeTodo);
+            ulEl.appendChild(todoLi);
+        }
+
+        function removeTodo(event){
+            var clickedLi = event.target;
+            clickedLi.parentNode.removeChild(clickedLi);
+        }
