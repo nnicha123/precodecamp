3ebcdd6339a60811e9146069af1781f33dcd4f3f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-23 10:11:08 +0700

    Change state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,14 +5,32 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+
+  // state available in component that extends from class
+  state = {
+    persons: [
+      {name:'Max', age:28},
+      {name:'Manu', age:29},
+      {name:'Stephanie', age:26}
+    ]
+  }
+  switchNameHandler = () => {
+    // DONT DO THIS this.state.persons[0].name = 'Maximillian'
+    this.setState({persons: [
+      {name:'Maximillan', age:28},
+      {name:'Manu', age:29},
+      {name:'Stephanie', age:27}
+    ]})
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App'));
