d04db5c74987e800a0d5c98af5659488622866e7
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-23 13:57:44 +0700

    Working with conditionals

--- day27/test/App.js
+++ day27/test/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-// Assignment part now in day27 folder
-// import UserOutput from './User/UserOutput';
-// import UserInput from './User/UserInput';
 
 class App extends Component {
   state = {
@@ -16,58 +13,45 @@ class App extends Component {
     showPersons: false
   }
 
-  // state = {
-  //   useroutputs:  {username : 'username'}
-  // }
-
-  switchNameHandler = (newName) => {
+  switchNameHandler = ( newName ) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    this.setState( {
       persons: [
         { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
-    })
+    } )
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
+  nameChangedHandler = ( event ) => {
+    this.setState( {
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 26 }
       ]
-    })
+    } )
   }
-
-  // Add event handler method to manipulate output
-  // userNameHandler = (event) => {
-  //   this.setState({
-  //     useroutputs:{username : event.target.value}
-  //   })
-  // }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState( { showPersons: !doesShow } );
   }
 
-  render() {
-
+  render () {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1px solid white',
-      boxShadow: '0 2rem 6rem rgba(0,0,0,0.3)',
+      border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
 
     let persons = null;
 
-    if(this.state.showPersons){
+    if ( this.state.showPersons ) {
       persons = (
         <div>
           <Person
@@ -76,7 +60,7 @@ class App extends Component {
           <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Max!')}
+            click={this.switchNameHandler.bind( this, 'Max!' )}
             changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
           <Person
             name={this.state.persons[2].name}
@@ -91,12 +75,9 @@ class App extends Component {
         <p>This is really working!</p>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
-        {/* Ternary expression */}
-
       </div>
-
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
