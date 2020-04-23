e3eb4a891a0c7ca651dc178e4895f1664b5d74f6
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-23 09:56:44 +0700

    Add props to component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <Person/>
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29"/>
+        <Person name="Stephanie" age="26"/>
       </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App'));
