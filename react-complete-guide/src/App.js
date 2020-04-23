f53657aa8876512ece14003955a2336d4182dfd4
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-23 09:46:02 +0700

    use class name so app.css now applied

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi I'm a React App</h1>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi I'm a React App</h1>
+    //   </div>
+    // );
+    return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App'));
   }
 }
 
