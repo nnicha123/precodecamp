aa2feda14acd5eef22e18d6699de643eb723f54e
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-24 16:33:24 +0700

    Using Refs and Prop-types

--- react-complete-guide/fromclass/src/components/Persons/Person/Person.js
+++ react-complete-guide/fromclass/src/components/Persons/Person/Person.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxillary';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.inputElementRef.current.focus();
+    }
     render(){
         console.log('[Person.js] rendering..');
     return (
@@ -13,6 +22,7 @@ class Person extends Component {
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
             <p>{this.props.children}</p>
             <input 
+            ref={this.inputElementRef}
             type="text" 
             onChange={this.props.changed} 
             value={this.props.name} />
@@ -20,6 +30,13 @@ class Person extends Component {
         );
     }
     
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed:PropTypes.func
 };
 
 export default withClass(Person,classes.Person);
--- react-complete-guide/fromclass/src/containers/App.js
+++ react-complete-guide/fromclass/src/containers/App.js
@@ -62,7 +62,13 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({persons:persons});
+    this.setState((prevState,props) => {
+      return {
+        persons:persons,
+        changeCounter: prevState.changeCounter+1
+      };
+      });
+    
 
     this.setState({
       persons: [
