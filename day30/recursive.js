de75c029559267ac9b5d4ef5f7f3fad6e12e327c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-26 11:45:10 +0700

    complete recursive

---
+++ day30/recursive.js
@@ -0,0 +1,11 @@
+function sum(arr, n) {
+    // Only change code below this line
+  if(n<=0){
+    return 0;
+  }
+  return sum(arr,n-1) + arr[n-1];
+    // Only change code above this line
+  }
+
+  console.log(sum([1,2,3,4,5,6,7], 5));
+  
