888c5037228868359fc8678213f1a9f706cc20cc
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-26 15:13:45 +0700

    arrays challenge js

---
+++ day30/arrayItems2.js
@@ -0,0 +1,14 @@
+function filteredArray(arr,elem){
+    let newArr = [];
+    for(let i = 0;i<arr.length;i++){
+        if(arr[i].indexOf(elem) === -1){
+            newArr.push(arr[i]);
+        }
+        
+    }
+    return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
+console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));
+
