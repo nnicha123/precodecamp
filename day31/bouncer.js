faf2463e527ff726b28603e5c1898e586204a2b1
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-27 11:51:45 +0700

    complete bouncer challenge

---
+++ day31/bouncer.js
@@ -0,0 +1,15 @@
+function bouncer(arr) {
+    let newArr = [];
+    for(let i=0;i<arr.length;i++){
+
+        if((arr[i])){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+  }
+  
+  console.log(bouncer([7, "ate", "", false, 9]));
+  console.log(bouncer(["a", "b", "c"]));
+  console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+  console.log(bouncer([null, NaN, 1, 2, undefined]));
