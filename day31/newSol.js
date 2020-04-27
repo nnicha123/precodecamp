b7ac97e130ba55f17588e1d3ba488874a52a9473
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-27 11:40:00 +0700

    finish new sol can add splice directly in there depending on the length of arr1

---
+++ day31/newSol.js
@@ -0,0 +1,11 @@
+function frankenSplice(arr1, arr2, n) {
+    // console.log(arr2.splice(n));
+    let newArr = arr2;
+    for(let i=0;i<arr1.length;i++){
+        newArr.splice(n,0,arr1[i]);
+        n++;
+    }
+    return newArr;
+  }
+  
+  console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
