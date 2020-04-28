698ca075cd74aefb9ab4fb300a56ad597167d27c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-28 09:56:08 +0700

    Outputting a burger

--- burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import classes from './BurgerIngredient.css';
 import PropTypes from 'prop-types';
 import { Component } from 'react';
@@ -34,6 +34,8 @@ class BurgerIngredient extends Component {
         default:
             ingredient = null;
         }
+
+        return ingredient;
 
     }
     
--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React,{ Component } from 'react';
 import Aux from '../../hoc/Auxillary';
+import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component{
     render(){
         return (
             <Aux>
-            <div>Burger</div>
+            <Burger/>
             <div>Build Controls</div>
             </Aux>
         );
