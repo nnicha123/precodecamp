cd4254f33e24e5a628b2f423f942fcec5912e1d2
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-28 10:14:43 +0700

    Update burger bui;der will check length of ingredients by using reduce methods and show message if none

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -2,12 +2,22 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_,i) => {
            return <BurgerIngredient key={igKey + i} type={igKey}/>
         })
-    });
+    })
+    .reduce((arr,el) => {
+        return arr.concat(el);
+    },[]);
+    console.log(transformedIngredients.length);
+    // check empty if empty show something
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p>Please add ingredients</p>
+    }
+
+    // reduce flattens the array
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
