9ced799a0c56a5b67bc91a862d0613e1f61b656b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-29 09:39:49 +0700

    use interceptors to index.js and add catch error to blog

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts : [],
-        selectedPostId:null
+        selectedPostId:null,
+        error:false
     }
 
     componentDidMount(){
@@ -26,19 +27,26 @@ class Blog extends Component {
                 this.setState({posts:updatedPosts})
                 // console.log(response)
             })
+            .catch(error => {
+                this.setState({error:true});
+            })
     }
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId:id});
     }
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-            key={post.id} 
-            title={post.title} 
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign:"center", color:"red"}}>Something went wrong!</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)}/>
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
--- http--01-starting-setup/src/index.js
+++ http--01-starting-setup/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+axios.interceptors.request.use(request => {
+    console.log(request);
+    // need to return request (can add something but need to return) otherwise will block
+    return request;
+}), error => {
+    console.log(error);
+    return Promise.reject(error);
+};
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // need to return request (can add something but need to return) otherwise will block
+    return response;
+}), error => {
+    console.log(error);
+    return Promise.reject(error);
+};
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
