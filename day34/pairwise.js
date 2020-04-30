c6c43e0530145cbb168d144ef0873c06a6e87bf5
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-30 14:18:50 +0700

    use index of old array and find the sum

--- day34/pairwise.js
+++ day34/pairwise.js
@@ -3,18 +3,16 @@ function pairwise(arr, arg) {
     let i = 0;
     let j = arr.length-1;
     let num =0;
-    // so old array don't get mutated
+    // so old array don't get mutated -> Will use old array later on for getting index
     let newArr = [...arr];
 
-    // Sort array first
+    // Sort array first to be able to use multiple pointers approach
     newArr.sort((a,b) => {
         return a===b? 0:a>b? 1:-1;
     });
     while(i<=j && i>=0 && j<=newArr.length-1){
         if((newArr[i] + newArr[j]) === arg){
-            num += (i+j);
-        
-            console.log(i,j);
+            num += (arr.indexOf(newArr[i])+arr.indexOf(newArr[j]));
             i++;
         } else if((newArr[i] + newArr[j]) <= arg){
             i++;
