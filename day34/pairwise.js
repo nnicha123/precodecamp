0755c5ac16ac1ca2b1843f99d98caa7689d73c8c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-30 14:14:20 +0700

    make a non-mutatable array as output

--- day34/pairwise.js
+++ day34/pairwise.js
@@ -3,16 +3,20 @@ function pairwise(arr, arg) {
     let i = 0;
     let j = arr.length-1;
     let num =0;
+    // so old array don't get mutated
+    let newArr = [...arr];
+
     // Sort array first
-    arr.sort((a,b) => {
+    newArr.sort((a,b) => {
         return a===b? 0:a>b? 1:-1;
     });
-    while(i<=j && i>=0 && j<=arr.length-1){
-        if((arr[i] + arr[j]) === arg){
+    while(i<=j && i>=0 && j<=newArr.length-1){
+        if((newArr[i] + newArr[j]) === arg){
             num += (i+j);
+        
             console.log(i,j);
             i++;
-        } else if((arr[i] + arr[j]) <= arg){
+        } else if((newArr[i] + newArr[j]) <= arg){
             i++;
         } else{
             j--;
