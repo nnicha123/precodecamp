30705ef74bfc10127bfb29f8d31e6985bf062033
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-04-30 12:13:16 +0700

    Parsed int corrected location

--- day34/inventory.js
+++ day34/inventory.js
@@ -5,20 +5,23 @@ function updateInventory(arr1, arr2) {
     let key = [];
     let value = [];
     for(let i =0;i<arr1.length;i++){
-        key[i] = arr1[i][1];
-        value[i] = arr1[i][0];
+        key[i] = arr1[i][0];
+        value[i] = arr1[i][1];
         obj1[key[i]] = value[i];
     }
 
     for(let i=0;i<arr2.length;i++){
-        if(obj1[arr2[i][1]]){
-            obj1[arr2[i][1]] += arr2[i][0];
+        if(obj1[arr2[i][0]]){
+            obj1[arr2[i][0]] += arr2[i][1];
         } else{
-            obj1[arr2[i][1]] = arr2[i][0];
+            obj1[arr2[i][0]] = arr2[i][1];
         }
     }
     // Convert object to array
     let newArr = Object.entries(obj1);
+    for(let i=0;i<newArr.length;i++){
+        newArr[i][0] = parseInt(newArr[i][0])
+    }
     return newArr;
 }
 
@@ -39,3 +42,5 @@ var newInv = [
 
 console.log(updateInventory(curInv, newInv));
 console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]));
+console.log(updateInventory([[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]], [])); //should return [[21, "Bowling Ball"], [2, "Dirty Sock"], [1, "Hair Pin"], [5, "Microphone"]].
+console.log(updateInventory([], [[2, "Hair Pin"], [3, "Half-Eaten Apple"], [67, "Bowling Ball"], [7, "Toothpaste"]]));
