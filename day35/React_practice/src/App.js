df631246da07986c09dd42bd87a5b0b2dfc7e535
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-01 14:56:19 +0700

    Add default files for react

---
+++ day35/React_practice/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import PizzaIngredient from './Pizza/PizzaIngredient';
+import PizzaTwo from './Pizzatwo';
+import classes from './App.css';
+import Heart from './heart';
+import Button from './Button';
+import ButtonCheese from './ButtonCheese';
+class App extends Component {
+  state = {
+      showPizza:false,
+      Cheese:false
+  };
+
+  addPizza = () => {
+    if(this.state.showPizza){
+      this.setState({showPizza:false})
+    }else{
+      this.setState({showPizza:true});
+    }
+  }
+
+  removeCheese = () => {
+    if(this.state.Cheese){
+      this.setState({Cheese:false})
+    }else{
+      this.setState({Cheese:true});
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div className={classes.App}>
+          <Button clicked={this.addPizza}/>
+          <ButtonCheese clicked={this.removeCheese}/>
+          <div className={classes.Pizza}>
+            <PizzaIngredient cheese={this.state.Cheese}/>
+          </div>
+          <div className={classes.PizzaTwo}>
+            <PizzaTwo show={this.state.showPizza}
+            cheese={this.state.Cheese}/>
+          </div>
+          <Heart className={classes.Heart} />
+        </div>
+      </div>
+    );
+
+  }
+}
+
+export default App;
---
+++ day35/React_practice/src/reactguide.js
@@ -0,0 +1,6 @@
+import React from 'react';
+function App() {
+    const greeting = 'Hello function component!';
+return <h1>{greeting}</h1>;
+}
+export default App;
