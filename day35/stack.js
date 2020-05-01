baa5a047a78a5bf5133413eba8086acf8d1ea53b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-01 10:40:48 +0700

    stack challenge

---
+++ day35/stack.js
@@ -0,0 +1,6 @@
+var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
+// Only change code below this line
+// Remove top element "PSY44" from stack
+homeworkStack.pop();
+homeworkStack.push("CS50");
+console.log(homeworkStack);
