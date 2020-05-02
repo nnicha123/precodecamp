6103034897f05e343d9482967b7cdd944fd19bae
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-02 11:28:27 +0700

    Add remove note and edit pages added event listener to title, body and remove element

---
+++ notes-app/notes-edit.js
@@ -0,0 +1,32 @@
+const titleELement = document.querySelector('#note-title')
+const bodyElement = document.querySelector('#note-body')
+const removeElement = document.querySelector('#remove-note')
+//Get id not including hash
+const noteId = location.hash.substring(1)
+const notes = getSavedNotes()
+const note = notes.find(function(note){
+    return note.id === noteId
+})
+
+if(note === undefined){
+    location.assign('./index.html')
+}
+
+titleELement.value = note.title
+bodyElement.value = note.body
+
+titleELement.addEventListener('input',function(e){
+    note.title = e.target.value
+    saveNotes(notes)
+})
+
+bodyElement.addEventListener('input',function(e){
+    note.body = e.target.value
+    saveNotes(notes)
+})
+
+removeElement.addEventListener('click',function(e){
+    removeNote(note.id)
+    saveNotes(notes)
+    location.assign('./index.html')
+})
