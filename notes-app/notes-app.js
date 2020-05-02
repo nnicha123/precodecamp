78a2de9a294c603a3f611c2ee08242a02f9c7807
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-02 13:44:36 +0700

    Moment challenge

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -32,13 +32,23 @@ window.addEventListener('storage',function(e){
     }
 })
 
-const now = moment()
-console.log(now.toString())
-// now.minute(5)
-now.add(1,'year').add(1,'minute').subtract(20,'days')
-console.log(now.toString())
-console.log(now.format('dddd Do MMM YYYY'))
-console.log(now.fromNow())
+// const now = moment()
+// console.log(now.toString())
+// now.add(1,'year').add(1,'minute').subtract(20,'days')
+// console.log(now.toString())
+// console.log(now.format('dddd Do MMM YYYY'))
+// console.log(now.fromNow())
+// const nowTimestamp = now.valueOf()
+// console.log(moment(nowTimestamp).toString())
+
+const birthday = new moment()
+birthday.year(1995)
+birthday.month(7)
+birthday.date(20)
+console.log(birthday.toString())
+
+console.log(birthday.format('MMM D, YYYY'));
+
 //localStorage.setItem('location','Bangkok');
 // console.log(localStorage.getItem('location'));
 // localStorage.removeItem('location')
