44f01ac7f8894078f98c8aba8e27e0801f0bf543
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-03 11:06:40 +0700

    change to class syntax

--- day37/person.js
+++ day37/person.js
@@ -1,31 +1,28 @@
-const Person = function(firstName,lastName,age,likes = []){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.likes = likes;
-}
-
-Person.prototype.getBio = function(){
-    let bio =  `${this.firstName} is ${this.age}.`
-    this.likes.forEach((like) => {
-        bio += `${this.firstName} likes ${like}.`
-    })
+class Person {
+    constructor(firstName,lastName,age,likes = []){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.likes = likes;
+    }
+    getBio(){
+            let bio =  `${this.firstName} is ${this.age}.`
+            this.likes.forEach((like) => {
+            bio += `${this.firstName} likes ${like}.`
+        })
     return bio
+    }
+
+    setName(fullName){
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]    
+    }
 }
 
-Person.prototype.setName = function(fullName){
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]    
-}
 
 const me = new Person('Nicha','Ngamtweerat',24,['Dogs','Coffee']);
 // me.setName('Opapa renu')
 console.log(me.getBio());
 const person2 = new Person('Clandey','Turner',51);
-Person.prototype.getBio = function(){
-    return 'Testing testing'
-}
-
-console.log(person2.getBio)
-console.log(me.getBio())
+console.log(person2.getBio())
