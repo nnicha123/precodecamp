74cc18c884f6da859ce64ac467375524a86cdbd8
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-03 11:22:08 +0700

    creating subclasses

--- day37/person.js
+++ day37/person.js
@@ -12,7 +12,6 @@ class Person {
         })
     return bio
     }
-
     setName(fullName){
         const names = fullName.split(' ')
         this.firstName = names[0]
@@ -20,9 +19,37 @@ class Person {
     }
 }
 
+class Employee extends Person{
+    constructor(firstName,lastName,age,position,likes){
+        super(firstName,lastName,age,likes)
+        this.position = position
+    }
+    getBio(){
+        return `${this.firstName} ${this.lastName} is a ${this.position}`
+    }
+}
 
-const me = new Person('Nicha','Ngamtweerat',24,['Dogs','Coffee']);
+class Student extends Person {
+    constructor(firstName,lastName,age,grade){
+        super(firstName,lastName,age)
+        this.grade = grade
+    }
+    getBio(){
+        if(this.grade >= 70){
+            return `${this.firstName} is passing the test`
+        }else{
+            return `${this.firstName} is failing the test`
+        }
+    }
+    updateGrade(amount){
+        return this.grade += amount
+    }
+}
+
+const me = new Employee('Nicha','Ngamtweerat',24,'Engineer',['Dogs','Coffee']);
 // me.setName('Opapa renu')
-console.log(me.getBio());
-const person2 = new Person('Clandey','Turner',51);
+// console.log(me.getBio());
+const person2 = new Student('Clandey','Turner',51,70);
+console.log(person2.getBio())
+person2.updateGrade(-10)
 console.log(person2.getBio())
