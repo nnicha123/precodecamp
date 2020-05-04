1aa468cc8cc8ce0293c1b8339f05c47f463e92a4
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-04 09:26:59 +0700

    Create a callback function

--- hangman/app.js
+++ hangman/app.js
@@ -16,31 +16,27 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle
     // guessesEl.textContent = game1.guessesLeft
     resultEl.textContent = game1.statusMessage
-})
 
-// Making an HTTP request
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange',(e)=>{
-    if(e.target.readyState === 4 && e.target.status === 200){
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if(e.target.readyState === 4){
-        console.log('An error has taken place')
-    }
-})
-request.open('GET','http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+    })
 
-const countryRequest = new XMLHttpRequest()
-countryRequest.addEventListener('readystatechange',(e)=>{
-    if(e.target.readyState ===4 && e.target.status === 200){
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((element) => element.alpha2Code == 'TH')
-        console.log(country.name)
-    } else if(e.target.readyState ===4){
-        console.log('Unable to fetch data')
-    }
-})
+    getPuzzle((error,puzzle) => {
+        if(error){
+            console.log(`Error: ${error}`)
+        } else{
+            console.log(puzzle)
+        }
+    })
 
-countryRequest.open('GET','http://restcountries.eu/rest/v2/all')
-countryRequest.send()
+// const countryRequest = new XMLHttpRequest()
+// countryRequest.addEventListener('readystatechange',(e)=>{
+//     if(e.target.readyState ===4 && e.target.status === 200){
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((element) => element.alpha2Code == 'TH')
+//         console.log(country)
+//     } else if(e.target.readyState ===4){
+//         console.log('Unable to fetch data')
+//     }
+// })
+
+// countryRequest.open('GET','http://restcountries.eu/rest/v2/all')
+// countryRequest.send()
--- hangman/request.js
+++ hangman/request.js
@@ -1,11 +1,15 @@
-// Making an HTTP request
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange',(e)=>{
-    if(e.target.readyState ===4 && e.target.status === 200){
-        const data = JSON.parse(e.target.alphaCode)
-        console.log(data)
-    }
-})
+const getPuzzle = (callback) => {
+    // Making an HTTP request
+    const request = new XMLHttpRequest()
+    request.addEventListener('readystatechange', (e) => {
+        if (e.target.readyState === 4 && e.target.status === 200) {
+            const data = JSON.parse(e.target.responseText)
+            callback(undefined,data.puzzle)
+        } else if (e.target.readyState === 4) {
+            callback('An error has taken place',undefined)
+        }
+    })
+    request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+    request.send()
+}
 
-request.open('GET','http://restcountries.eu/rest/v2/all')
-request.send()
