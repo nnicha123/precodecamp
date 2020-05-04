c4a4154fd2cd1ae07a005712454f14a220cc8385
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-04 08:39:24 +0700

    how to initialise HTTP request from javascript code

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hyper Text Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const resultEl = document.querySelector('#result')
@@ -13,3 +17,14 @@ window.addEventListener('keypress', function (e) {
     // guessesEl.textContent = game1.guessesLeft
     resultEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange',(e)=>{
+    if(e.target.readyState === 4){
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    }
+})
+request.open('GET','http://puzzle.mead.io/puzzle')
+request.send()
