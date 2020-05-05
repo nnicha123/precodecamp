b42729f896ba41e981668ad11f27f25919a1950c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-05 14:41:09 +0700

    Linking class name to input

--- day39/dynamic_styling/app.js
+++ day39/dynamic_styling/app.js
@@ -1,6 +1,15 @@
 new Vue({
     el:'#app',
     data:{
-        attachRed:false
+        attachRed:false,
+        color:'green'
+    },
+    computed:{
+        divClasses:function(){
+            return{
+                red:this.attachRed,
+                blue:!this.attachRed
+            }
+        }
     }
 });
