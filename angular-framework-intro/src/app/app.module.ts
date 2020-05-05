23ec9aeacdf8e188636a65fdcbc424ce9b701696
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-05 10:49:12 +0700

    Added account and blog and then modify to provide links and routes

--- angular-framework-intro/src/app/app.module.ts
+++ angular-framework-intro/src/app/app.module.ts
@@ -8,19 +8,30 @@ import { UsernameComponent } from './username/username.component';
 import {HobbyComponent} from './hobby/hobby.component';
 import { InfoComponent } from './info/info.component';
 import { UserComponent } from './user/user.component';
+import { BlogComponent } from './blog/blog.component';
+import { AccountComponent } from './account/account.component';
+import { RouterModule,Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  {path:'',component:UserComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'account',component:AccountComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
     HobbyComponent,
     UsernameComponent,
     InfoComponent,
-    UserComponent
+    UserComponent,
+    BlogComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
