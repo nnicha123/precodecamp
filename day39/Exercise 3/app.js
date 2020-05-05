ffbfa64521c245a2c8780667c3dc9f098f50ca4b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-05 14:28:07 +0700

    Finish assignment 3, using watch and computed

--- day39/Exercise 3/app.js
+++ day39/Exercise 3/app.js
@@ -2,5 +2,18 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed:{
+            result:function(){
+                return this.value <= 37? 'not there yet':'done';
+            }
+        },
+        watch:{
+            value:function(value){
+                var vm = this;
+                setTimeout(function(){
+                    vm.value = 0;
+                },5000)
+            }
         }
     });
