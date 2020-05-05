36fca951af2fe49970563c7f5c6f85d5a1ad2b97
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-05 14:14:23 +0700

    Record class work here

---
+++ vue-framework-intro/src/components/Class.vue
@@ -0,0 +1,31 @@
+<template>
+
+<div id="class">
+<button v-on:click="increase(2,$event)">Click me</button>
+<p>{{counter}}</p>
+<p v-on:mousemove="updateCoordinates">
+  Coordinates: {{x}} / {{y}} - <span v-on:mousemove.stop="">DEAD SPOT</span>
+</p>
+<input type="text" v-on:keyup.enter.space="alertMe">
+</div>
+</template>
+new Vue ({
+	el:'#app',
+  data: {
+  	counter:0,
+    x:0,
+    y:0
+  },
+  methods:{
+  	increase:function(step,event){
+    	this.counter += step;
+    },
+    updateCoordinates: function(event){
+    	this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function(){
+    	alert('Alert!');
+    }
+  }
+})
---
+++ vue-framework-intro/src/components/ClassRecord2.vue
@@ -0,0 +1,60 @@
+// HTML
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<div id="app">
+<button @click="changeLink">Click to change link</button>
+<a :href="link">Link</a>
+<button @click="counter++">Increase</button>
+<button @click="counter--">Decrease</button>
+<button @click="secondCounter++">Increase Second</button>
+<p>{{counter}} | {{secondCounter}}</p>
+<p>{{result()}} | {{output}}</p>
+<p @mousemove="updateCoordinates">
+  Coordinates: {{x}} / {{y}} - <span @mousemove.stop="">DEAD SPOT</span>
+</p>
+<input type="text" @keyup.enter.space="alertMe" v-model="name">
+<p>{{name}}</p>
+</div>
+ 
+// Vue
+new Vue ({
+	el:'#app',
+  data: {
+  	counter:0,
+    x:0,
+    y:0,
+    name:'Nicha',
+    secondCounter:0,
+    link:'http://google.com'
+  },
+  watch:{
+  	counter: function(value){
+    var vm = this;
+    	setTimeout(function(){
+      	vm.counter = 0;
+        vm.secondCounter = 0;
+      },2000)
+    }
+  },
+  computed:{
+  	output:function(){
+    console.log('Computed');
+    return this.counter > 5 ? 'Greater 5':'Smaller 5';
+    }
+  },
+  methods:{
+    updateCoordinates: function(event){
+    	this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    result:function(){
+     console.log('Method');
+    return this.counter >5 ? 'Greater 5' :'Smaller 5';
+    },
+    alertMe: function(){
+    	alert('Alert!');
+    },
+    changeLink:function(){
+    	this.link = 'http//apple.com'
+    }
+  }
+})
