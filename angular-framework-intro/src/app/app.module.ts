ca32ae540f2ed9953a736f004d8b9a40811c4281
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-05 10:33:41 +0700

    Added 2 components using the command -> ng g c user -> ng g c info

--- angular-framework-intro/src/app/app.module.ts
+++ angular-framework-intro/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsernameComponent } from './username/username.component';
 import {HobbyComponent} from './hobby/hobby.component';
+import { InfoComponent } from './info/info.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HobbyComponent,
-    UsernameComponent
+    UsernameComponent,
+    InfoComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
