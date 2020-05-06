685d03390167b77b4a31f0a51cb7d9cec10d670c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-06 11:48:42 +0700

    setting up components locally

--- day40/componentintro/app.js
+++ day40/componentintro/app.js
@@ -1,13 +1,30 @@
-Vue.component('my-component',{
+// Pointer behavior changing one will change both because they share data source (if using the method below)
+//var data = {status : 'Critical'}
+
+var cmp = {
     data:function(){
-        return{
+        return {
             status:'Critical'
         }
     },
-    template:'<p>Server Status : {{status}}</p>'
+    template:'<p>Server Status : {{status}}(<button @click="changeStatus">Change</button>)</p>',
+    methods:{
+        changeStatus(){
+            this.status = 'Normal'
+        }
+    }
+}
+
+new Vue({
+    el:'#app',
+    components:{
+        'my-component' :cmp
+    }
 })
 
 new Vue({
-    el:'#app'
-    
+    el:'#app2',
+    components:{
+        'my-component' :cmp
+    }
 })
