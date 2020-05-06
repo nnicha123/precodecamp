744de652b17b1a1a20e6dead62ef52bcfd9f1bae
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-06 09:25:52 +0700

    Attack button

--- Monster/app.js
+++ Monster/app.js
@@ -10,6 +10,37 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth =100;
             this.monsterHealth = 100;
+        },
+        attack:function(){
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random()*max) + 1,min);
+            this.monsterHealth -= damage;
+            if(this.monsterHealth <= 0){
+                alert('YOU WON!')
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random()*max) + 1,min);
+            this.playerHealth -= damage;
+            if(this.playerHealth <= 0){
+                alert('YOU LOSE!')
+                this.gameIsRunning = false;
+                return;
+            }
+        },
+        specialAttack:function(){
+
+        },
+        heal:function(){
+
+        },
+        giveUp:function(){
+            this.gameIsRunning = false;
+            this.playerHealth = 0;
         }
     }
 });
