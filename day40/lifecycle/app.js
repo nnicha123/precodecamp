30bc87ef1ccfea089b178caf6209d7591aa637f6
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-06 11:01:30 +0700

    Instance lifeCycle practice

---
+++ day40/lifecycle/app.js
@@ -0,0 +1,35 @@
+new Vue({
+    el:'#app',
+    data: {
+        title:'The VueJS Instance'
+    },
+    beforeCreate() {
+        console.log('beforeCreate()')
+    },
+    created() {
+        console.log('created()')
+    },
+    beforeMount() {
+        console.log('beforeMount()')
+    },
+    mounted() {
+        console.log('mounted()')
+    },
+    beforeUpdate() {
+        console.log('beforeUpdate()')
+    },
+    updated() {
+        console.log('updated()')
+    },
+    beforeDestroy() {
+        console.log('beforeDestroy()')
+    },
+    destroyed() {
+        console.log('destroyed()')
+    },
+    methods: {
+        destroy:function(){
+            this.$destroy();
+        }
+    },
+})
