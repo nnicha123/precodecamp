6b543b0ed78ccf3eccc9bddb3177709728b6a491
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-07 09:30:32 +0700

    Added changes to new quotes project

--- Quote/src/App.vue
+++ Quote/src/App.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-    
+        <app-quote>Hello</app-quote>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from '../components/QuoteGrid.vue'
+    import Quote from '../components/Quote.vue'
     export default {
-        
+
+        data:function(){
+            return{
+                quote:['Just a Quote to see something'],
+                maxQuotes:10
+            }
+        },
+        components:{
+            appQuoteGrid:QuoteGrid,
+            appQuote: Quote
+        }
     }
 </script>
 
