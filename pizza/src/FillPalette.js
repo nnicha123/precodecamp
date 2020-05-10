2a3e2c2afd8303134c532e9e7784ec26ca773169
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-10 10:33:16 +0700

    Change button styling from adding color palette features

--- pizza/src/FillPalette.js
+++ pizza/src/FillPalette.js
@@ -4,12 +4,12 @@ import classes from '../src/FillPalette.css'
 const fillCake = (props) => (
     <div>
         <h2>Choose Color from Palette</h2>
-        <div className={classes.pink}></div>
-        <div className={classes.red}></div>
-        <div className={classes.orange}></div>
-        <div className={classes.yellow}></div>
-        <div className={classes.green}></div>
-        <div className={classes.blue}></div>
+        <button className={classes.pink}></button>
+        <button className={classes.red}></button>
+        <button className={classes.orange}></button>
+        <button className={classes.yellow}></button>
+        <button className={classes.green}></button>
+        <button className={classes.blue}></button>
     </div>
 )
 
