1b77997b77c7a645d2ce3fa52001e23427cf50cf
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-10 10:29:03 +0700

    Correctly position palette into pizza page

--- pizza/src/FillPalette.js
+++ pizza/src/FillPalette.js
@@ -3,6 +3,7 @@ import classes from '../src/FillPalette.css'
 
 const fillCake = (props) => (
     <div>
+        <h2>Choose Color from Palette</h2>
         <div className={classes.pink}></div>
         <div className={classes.red}></div>
         <div className={classes.orange}></div>
