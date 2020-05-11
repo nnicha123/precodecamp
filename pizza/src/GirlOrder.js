4bf9f778c19a3503568ee2e8d857bd56d939360e
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-11 11:33:40 +0700

    Added girl head and body

--- pizza/src/GirlOrder.js
+++ pizza/src/GirlOrder.js
@@ -6,8 +6,10 @@ const girl = (props) => (
         <div className={classes.Face}></div>
         <div className={classes.Hair}></div>
         <div className={classes.Eyes}></div>
+        <div className={classes.Pupil}></div>
         <div className={classes.Mouth}></div>
-        <div style={{transform:'scale(1.5)'}} className={classes.Skirt}></div>
+        <div style={{transform:'scale(1.5)'}} className={classes.Shirt}></div>
+        <div className={classes.Skirt}></div>
     </div>
 )
 
