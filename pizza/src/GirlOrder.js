94bb9476c9e81ee9454950d04d532c9747c38bce
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-11 11:09:46 +0700

    Try to add a character

---
+++ pizza/src/GirlOrder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from '../src/GirlOrder.css';
+
+const girl = (props) => (
+    <div>
+        <div className={classes.Face}></div>
+        <div className={classes.Hair}></div>
+        <div className={classes.Eyes}></div>
+        <div className={classes.Mouth}></div>
+    </div>
+)
+
+export default girl
