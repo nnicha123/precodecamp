df1880b9e87bd0e260470576db37ef11ed9416a4
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-12 09:17:59 +0700

    Random text generator added (can choose length)

--- Spelling/app.js
+++ Spelling/app.js
@@ -2,7 +2,8 @@ new Vue({
     el:'#vue-app',
     data:{
         text:'',
-        arr:[]
+        arr:[],
+        random:''
     },
     methods:{
         splittext:function(){
@@ -12,6 +13,9 @@ new Vue({
         removeText:function(el){
             findIndex = this.arr.indexOf(el)
             this.arr.splice(findIndex,1)
+        },
+        randomText:function(length){
+            this.random = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, length).toUpperCase();
         }
     },
     computed: {
