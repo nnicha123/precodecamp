5b7aba304b32d2ee93b7c3cc1ee65c0ea6ed43eb
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-12 14:06:00 +0700

    Finished game -> need to modify other stuff

--- Spelling/app.js
+++ Spelling/app.js
@@ -5,21 +5,25 @@ new Vue({
         arr:[],
         random:'',
         input:'',
-        countDown:10,
+        countDown:20,
         gameOver:false,
+        youWin:false
     },
     methods:{
         splittext:function(){
             this.arr = this.text.split('')
-
+            console.log()
         },
         removeText:function(el){
             findIndex = this.arr.indexOf(el)
             this.arr.splice(findIndex,1)
         },
         randomText:function(length){
+            this.countDown = 20;
             this.text = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, length).toUpperCase();
             this.arr = this.text.split('')
+            this.gameOver = false;
+            this.youWin = false;
         },
         testInput:function(){
             letterIndex = this.text.indexOf(this.input)
@@ -34,17 +38,22 @@ new Vue({
         },
         countDownTimer:function(){
             if(this.countDown > 0){
+                if(this.arr.length ===0 ){
+                    this.youWin = true;
+                    this.countDown=0;
+                }
                 setTimeout(() => {
                     this.countDown -=1
                     this.countDownTimer()
                 },1000)
+                
             }else{
-                this.gameOver = true;
+                this.gameOver = true
             }
         },
         resetTimer:function(){
             this.gameOver = false;
-            this.countDown = 10;
+            this.countDown = 20;
         }
     },
     computed: {
