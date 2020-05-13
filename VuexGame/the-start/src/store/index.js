3ad3de35adbaa5b7ef828b2c03e3a01c72de1c13
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-13 10:03:06 +0700

    deleting files and setting up intitlal states

--- VuexGame/the-start/src/store/index.js
+++ VuexGame/the-start/src/store/index.js
@@ -1,15 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Vuex is the state management system
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    bytes:0,
+    totalBytes:0,
+    bps:0,
+    bpk:1,
+    player:{
+      name:"",
+      level:0,
+      nextLevel:100,
+      increase:3
+    },
+    power_ups:[],
+    updates:[
+
+    {
+      name:"Intern",
+      cost:50,
+      increase:1.15,
+      bps:0.1,
+      quantity:0,
+      unlocksAt:0
+    },
+    {
+      name:"Junior Developer",
+      cost:100,
+      increase:1.2,
+      bps:0.5,
+      quantity:0,
+      unlocksAt:1
+    },
+    {
+      name:"Web Developer",
+      cost:300,
+      increase:1.4,
+      bps:1,
+      quantity:0,
+      unlocksAt:2
+    }
+    
+    ]
   },
   mutations: {
   },
-  actions: {
-  },
-  modules: {
+  getters: {
+
   }
 })
