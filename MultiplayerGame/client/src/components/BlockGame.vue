8644ac5ae77aed1eccfa3eb431c9b4545bc173de
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-13 09:40:48 +0700

    Move using the app.js from server

--- MultiplayerGame/client/src/components/BlockGame.vue
+++ MultiplayerGame/client/src/components/BlockGame.vue
@@ -6,6 +6,12 @@
       height="480"
       style="border:1px solid black;">
     </canvas>
+    <p>
+      <button v-on:click="move('right')">Right</button>
+      <button v-on:click="move('left')">Left</button>
+      <button v-on:click="move('up')">Up</button>
+      <button v-on:click="move('down')">Down</button>
+    </p>
   </div>
 </template>
 
@@ -28,8 +34,17 @@ export default {
   },
   mounted() {
     this.context = this.$refs.game.getContext("2d");
-    this.context.fillRect(this.position.x, this.position.y,20,20)
+    this.socket.on("position",data => {
+        this.position = data
+        this.context.clearRect(0,0,this.$refs.game.width,this.$refs.game.height)
+        this.context.fillRect(this.position.x, this.position.y,20,20)
+    })
   },
+  methods:{
+    move(direction){
+      this.socket.emit("move",direction)
+    }
+  }
 }
 </script>
 
