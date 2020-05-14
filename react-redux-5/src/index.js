188ece049977b1b4a47420b6e8b6b278a1e3e491
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-14 14:48:39 +0700

    default setups -> added store and reducer to store states

--- react-redux-5/src/index.js
+++ react-redux-5/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducer';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
+
+ 
 ReactDOM.render(
   <React.StrictMode>
     <App />
