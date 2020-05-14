f12ef92f6daa1f00ff8ebad55899a3a828373f5b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-14 09:45:37 +0700

    connect functionalities of App.js to the reducer.js file, and added functionality in the reducer file

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,33 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux';
 
 class App extends Component {
 
-  state = {
-    age:21
-  }
-  onAgeUp = () => {
-    this.setState({
-      ...this.state,
-      age:++this.state.age
-    })
-  }
-  onAgeDown = () => {
-    this.setState({
-      ...this.state,
-      age:--this.state.age
-    })
-  }
   render() {
     return (
       <div className="App">
-        <div>Age: <span>{this.state.age}</span></div>
-        <button onClick={this.onAgeUp}>Age UP</button>
-        <button onClick={this.onAgeDown}>Age DOWN</button>
+        <div>Age: <span>{this.props.age}</span></div>
+        <button onClick={this.props.onAgeUp}>Age UP</button>
+        <button onClick={this.props.onAgeDown}>Age DOWN</button>
       </div>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    age:state.age
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onAgeUp: () => dispatch({type:'AGE_UP'}), 
+    onAgeDown: () => dispatch({type:'AGE_DOWN'})
+  }
+}
 
-export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- react-redux-tutorial/src/store/reducer.js
+++ react-redux-tutorial/src/store/reducer.js
@@ -2,5 +2,14 @@ const initialState = {
     age:21
 };
 const reducer = (state = initialState,action) => {
-
-}
+    // create a copy of state
+    const newState = {...state};
+    if(action.type === 'AGE_UP'){
+        newState.age++;
+    }
+    if(action.type === 'AGE_DOWN'){
+        newState.age--
+    }
+    return newState;
+};
+export default reducer;
