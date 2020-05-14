2413efeea15d7146c730821352cdeaf0e820ee0b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-14 15:00:28 +0700

    Correctly coded app.js and reducer and added dependencies on index.js

--- react-redux-5/src/App.js
+++ react-redux-5/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import {connect} from 'react-redux';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="col">
           <div><span>A:</span><span>{this.props.a}</span></div>
-          <button>Update A</button>
+          <button onClick={this.props.updateA}>Update A</button>
         </div>
         <div className="col">
           <div><span>B:</span><span>{this.props.b}</span></div>
-          <button>Update A</button>
+          <button onClick={this.props.updateB}>Update B</button>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStoreToProps = (store) => {
+  return {
+    a:store.a,
+    b:store.b
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return{
+    updateA: () => dispatch({type:'UPDATE_A'}),
+    updateB: () => dispatch({type:'UPDATE_B'})
+  }
+}
+
+export default connect(mapStoreToProps,mapDispatchToProps)(App);
--- react-redux-5/src/store/reducer.js
+++ react-redux-5/src/store/reducer.js
@@ -4,7 +4,18 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-
+    if(action.type === 'UPDATE_A'){
+        return {
+            ...state,
+            a:state.a + state.b
+        }
+    }
+    if(action.type === 'UPDATE_B'){
+        return {
+            ...state,
+            b:state.a + state.b
+        }
+    }
 
     return state
 }
