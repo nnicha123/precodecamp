9bf3cca8c43a55bd51d7f28b5d0efb738a86cc9f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-14 15:30:55 +0700

    Relocate folders and continue

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reducer from './store/reducer';
+import reducer from './store/reducers/reducer';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
