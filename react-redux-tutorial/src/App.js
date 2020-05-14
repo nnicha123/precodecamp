f6d2abe55160364f9012e3bf4a5b7e288d5d26ba
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-14 09:28:19 +0700

    Installed react-redux (but first needed to rename project name to react-redux-tutorial)

---
+++ react-redux-tutorial/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    age:21
+  }
+  onAgeUp = () => {
+    this.setState({
+      ...this.state,
+      age:++this.state.age
+    })
+  }
+  onAgeDown = () => {
+    this.setState({
+      ...this.state,
+      age:--this.state.age
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div>Age: <span>{this.state.age}</span></div>
+        <button onClick={this.onAgeUp}>Age UP</button>
+        <button onClick={this.onAgeDown}>Age DOWN</button>
+      </div>
+    )
+  }
+}
+
+export default App;
