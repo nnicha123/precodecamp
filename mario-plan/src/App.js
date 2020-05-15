e41165dc1285737b7dffa983ee2e2a87bac6721c
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-15 09:15:34 +0700

    Dashboard notifications and project list

--- mario-plan/src/App.js
+++ mario-plan/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch,Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-
+import DashBoard from './components/dashboard/Dashboard';
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar/>
+          {/* Make sure only one route loaded at a time */}
+          <Switch>
+            <Route path="/" component={DashBoard}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ mario-plan/src/components/project/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by NetNinja</p>
+                    <p className="grey-text">15 May, 9.08am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by NetNinja</p>
+                    <p className="grey-text">15 May, 9.08am</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by NetNinja</p>
+                    <p className="grey-text">15 May, 9.08am</p>
+                </div>
+            </div>
+
+        </div>
+    )   
+}
+export default ProjectList;
