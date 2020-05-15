232fe14cc55cf7d4341aae5e2749dfe8042ab1e7
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-15 08:45:35 +0700

    setup to use routes inside app

--- mario-plan/src/App.js
+++ mario-plan/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>MarioPlan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>MarioPlan</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
