c8388ecde813e616d77dc0ba84a40895b173699d
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-16 15:33:00 +0700

    Scaffold and AppBar Widgets

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hey ninjas!'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('my first flutter app'),
+      centerTitle: true,
+    ),
+    body: Center(
+       child: Text('Hello ninjas!'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+    ),
+  ),
 ));
 
 
