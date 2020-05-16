7997a311aafda8515a8d96db0ddc67aeaa29846a
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-16 15:51:23 +0700

    Changed pubspec and main.dart

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('my first flutter app'),
       centerTitle: true,
+      backgroundColor: Colors.pinkAccent,
     ),
     body: Center(
-       child: Text('Hello ninjas!'),
+       child: Text(
+         'Hello Ninjas!',
+         style: TextStyle(
+           fontSize: 20.0,
+           fontWeight: FontWeight.bold,
+           letterSpacing: 2.0,
+           color: Colors.grey[600],
+           fontFamily: 'IndieFlower',
+         ),
+       ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
       child: Text('click'),
+      backgroundColor: Colors.pink,
     ),
   ),
 ));
