9dec847156ced64b63986df457e0571980c01c09
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-16 16:02:46 +0700

    Update to create on stateless widget (states doesn't change continously) to avoid having to rerun that green button -> only saving is enough)

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,31 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('my first flutter app'),
-      centerTitle: true,
-      backgroundColor: Colors.pinkAccent,
-    ),
-    body: Center(
-       child: Text(
-         'Hello Ninjas!',
-         style: TextStyle(
-           fontSize: 20.0,
-           fontWeight: FontWeight.bold,
-           letterSpacing: 2.0,
-           color: Colors.grey[600],
-           fontFamily: 'IndieFlower',
-         ),
-       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){},
-      child: Text('click'),
-      backgroundColor: Colors.pink,
-    ),
-  ),
+  home:Home()
 ));
 
-
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first flutter app'),
+        centerTitle: true,
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Center(
+        child: Text(
+          'Hello Ninjas!',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Text('click'),
+        backgroundColor: Colors.pink,
+      ),
+    );
+  }
+}
 
