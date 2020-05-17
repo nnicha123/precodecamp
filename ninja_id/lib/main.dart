949cfe4132f04075a7b4412af5daa6ccae8eac66
nnicha123 <nicha_ngamtweerat@hotmail.com>
2020-05-17 14:53:22 +0700

    Create new flutter project (Ninja ID)

---
+++ ninja_id/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:NinjaCard(),
+));
+class NinjaCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ninja ID Card'),
+      ),
+    );
+  }
+}
+
+
